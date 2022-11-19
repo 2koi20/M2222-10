1a9f239bd28acd950fe27adb6cf1b28ffb745c69
Khoi Pham <khoi.pham@student.allenisd.org>
2022-11-18 22:04:51 -0600

    first commit w old code

---
+++ C101 Factorial [Swift]/.merlin/build/main.swift
@@ -0,0 +1,22 @@
+
+
+// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+let n = 16
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+
+
+// Add your code below:
+func factorial(n:Int) -> Int {
+  print("Calculating factorial(\(n))...")
+  if n <= 1 {
+    print("Terminal case, returning 1.")
+    return 1
+  } else {
+    let i = (n * factorial(n:(n-1)))    
+    print("Non-terminal case, returning \(i).")
+    return (i)
+  }
+} 
+print("The final result is: \(factorial(n:n)).")
